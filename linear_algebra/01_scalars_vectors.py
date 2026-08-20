a6c04e2d9468b89c10c1f0945e6084509a0cb3d2
RitaM736 <78433868+RitaM736@users.noreply.github.com>
2026-08-20 12:03:04 +0300

    Add basic vector operations using NumPy
    Scalars & Vectors with NumPy

---
+++ linear_algebra/01_scalars_vectors.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+v = np.array([2, 4, 6, 8])
+
+print("Vector:", v)
+print("Shape:", v.shape)
+print("Dimensions:", v.ndim)
+print("Size:", v.size)
+print("Mean:", v.mean())
+print("Sum:", v.sum())
+print("Min:", v.min())
+print("Max:", v.max())
